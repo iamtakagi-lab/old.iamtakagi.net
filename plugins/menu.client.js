f7157bccac8c478bbacfd4f1dcb5137140f8b796
notsmatch <smatchjp@gmail.com>
2020-07-02 03:22:26 +0900

    fix:

--- plugins/menu.client.js
+++ plugins/menu.client.js
@@ -1,6 +1,6 @@
-export default ({ app, store }) => {
+export default ({ $app, store }) => {
     // For mobile navigation when clicking on a menu link
-    app.router.afterEach(() => {
+    $app.router.afterEach(() => {
       if (store.state.menu.open) {
         setTimeout(() => store.commit('menu/close'), 10)
       }
