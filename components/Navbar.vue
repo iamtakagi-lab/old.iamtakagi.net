3024c44250fd82debe2d45cedd008032024967fd
notsmatch <smatchjp@gmail.com>
2020-07-02 02:06:26 +0900

    feat: navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav
+    class="fixed top-0 z-40 w-full border-b dark:border-gray-800 bg-white dark:bg-gray-900"
+    @click="scrollToTop"
+  >
+    <div class="container mx-auto px-4 lg:px-8 flex-1">
+      <div class="flex items-center justify-between h-16">
+        <div class="w-1/6" @click.stop="noop">
+          <NuxtLink
+            to="/"
+            class="text-xl font-bold tracking-tight flex items-center flex-shrink-0"
+            style="width: 104px;"
+            aria-label="Nuxt Content Logo"
+          >たかぎめも</NuxtLink>
+        </div>
+
+        <div class="flex items-center justify-end w-1/6">
+          <button
+            class="lg:hidden p-2 rounded-md hover:text-green-500 focus:outline-none focus:outline-none -mr-2"
+            aria-label="Hamburger Menu"
+            @click.stop="menu = !menu"
+          >
+            <IconX v-if="menu" class="w-6 h-6" />
+            <IconMenu v-else class="w-6 h-6" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  computed: {
+    menu: {
+      get() {
+        return this.$store.state.menu.open;
+      },
+      set(val) {
+        this.$store.commit("menu/toggle", val);
+      }
+    }
+  },
+  methods: {
+    scrollToTop() {
+      if (window.innerWidth >= 1280) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    },
+    noop() {}
+  }
+};
+</script>
