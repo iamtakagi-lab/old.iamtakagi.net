979e7edb40719cc9ecb128bd22db6e4cf2556c21
riptakagi <smatchjp@gmail.com>
2020-07-03 16:15:12 +0900

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
    <footer class="h-24 w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900">
     <div class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between">
       <div class="flex items-center">
-        <a class="mr-2 text-sm leading-none" href="https://riptakagi.netlify.app" target="_blank" rel="noopener">
+        <a class="mr-2 text-sm leading-none" href="https://riptakagi.me" target="_blank" rel="noopener">
         &copy; {{ new Date().getFullYear() }} riptakagi
         </a>
       </div>
