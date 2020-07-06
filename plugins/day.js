9c746998e65c9dad2800714552e8da9bfb47ddf1
notsmatch <smatchjp@gmail.com>
2020-07-07 03:19:47 +0900

    fix: dayjs 日本語locale

--- plugins/day.js
+++ plugins/day.js
@@ -1,4 +1,9 @@
 import dayjs from 'dayjs'
+import 'dayjs/locale/ja';
+
+// 日本語表示
+import 'dayjs/locale/ja'
+dayjs.locale('ja')
 
 export default ({ app }, inject) => {
    inject('dayjs', ((string) => dayjs(string)))
