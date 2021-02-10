80929e677489825254f87211e73f8868d2f72587
Takagi <smatchjp@gmail.com>
2021-02-10 18:28:22 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    titleTemplate: '%s / たかぎめも',
+    titleTemplate: '%s / Takagi',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
