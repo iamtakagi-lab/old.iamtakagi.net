c63d3ca789833531257d78f6736676298f1a9a92
Takagi <smatchjp@gmail.com>
2021-02-11 12:52:12 +0900

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,14 +3,8 @@
     <div class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between">
       <div class="flex items-center">
         <a class="mr-2 text-sm leading-none" href="https://riptakagi.me" target="_blank" rel="noopener">
-        &copy; {{ new Date().getFullYear() }} たかぎめも
+        &copy; 2020-{{ new Date().getFullYear() }} Takagi
         </a>
-      </div>
-
-      <div class="flex items-center">
-        <nuxt-link to="/7" class="mr-2 text-sm leading-none">
-        プライバシーポリシー
-        </nuxt-link>
       </div>
     </div>
   </footer>
